4463c9260ec98a3294fedca02a245266f1c405ba
AnketKohak <anketkohak40@gmail.com>
2024-09-11 19:06:11 +0530

    Initial Commit

---
+++ ShortsPlayer/ShortsPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShortsPlayerApp.swift
+//  ShortsPlayer
+//
+//  Created by Anket Kohak on 11/09/24.
+//
+
+import SwiftUI
+
+@main
+struct ShortsPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
