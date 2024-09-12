97a4a4a21ea12f2869c18f27e8c79b7ee0075ee5
AnketKohak <anketkohak40@gmail.com>
2024-09-12 11:02:44 +0530

    gird Layout added 38.56

--- ShortsPlayer/VideoManager.swift
+++ ShortsPlayer/VideoManager.swift
@@ -10,13 +10,52 @@ import Foundation
 enum Query: String,CaseIterable{
     case nature, animals, people, ocean, food
 }
+class VideoManager:ObservableObject {
+    
+    
+    @Published private(set) var videos : [Video] = []
+    @Published var selectedQuery: Query = Query.nature {
+        didSet{
+            Task.init {
+                await findVideos(topic: selectedQuery)
+                
+            }
+        }
+    }
+    init(){
+        Task.init{
+            await findVideos(topic: selectedQuery)
+            
+        }
+    }
+    
+    func findVideos(topic : Query) async{
+        do {
+            guard let url = URL(string: "https://api.pexels.com/videos/search?query=\(topic)&per_page=10&orientation=portrait") else{  fatalError("Missing url") }
+            var urlRequest = URLRequest(url:url)
+            urlRequest.setValue("98lZe1wsrPmg2lbi64qZver2vBjlXNmhVOevhPxcVnnbeSDct8LgifjX", forHTTPHeaderField: "Authorization")
+            
+          let (data,response) = try await URLSession.shared.data(for: urlRequest)
+            guard (response as? HTTPURLResponse)?.statusCode == 200 else { fatalError("Error while fetching data") }
+            
+            let decoder = JSONDecoder()
+            decoder.keyDecodingStrategy = .convertFromSnakeCase
+            let decodeData = try decoder.decode(ResponseBody.self, from: data)
+            self.videos = []
+            self.videos = decodeData.videos
+        } catch{
+            print("error fetching data from Pexels: \(error)")
+            
+        }
+    }
+}
 
 struct ResponseBody: Decodable{
     var page: Int
     var perPage:Int
     var totalResults:Int
     var url:String
-    var video:[Video]
+    var videos:[Video]
     
 }
 
--- ShortsPlayer/View/ContentView.swift
+++ ShortsPlayer/View/ContentView.swift
@@ -8,22 +8,36 @@
 import SwiftUI
 
 struct ContentView: View {
+    @StateObject var videoManager = VideoManager()
+    var columns = [GridItem(.adaptive(minimum: 160),spacing: 20)]
     var body: some View {
-        VStack {
-            HStack{
-                ForEach(Query.allCases,id: \.self){
-                    searchQuery in
-                    QueryTag(query: searchQuery, isSelected:false)
+        NavigationView{
+            VStack {
+                HStack{
+                    ForEach(Query.allCases,id: \.self){
+                        searchQuery in
+                        QueryTag(query: searchQuery, isSelected:false)
+                        
+                    }
                     
                 }
-                
+                ScrollView{
+                    LazyVGrid(columns: columns, spacing: 20){
+                        ForEach(videoManager.videos, id: \.id){ video
+                            in
+                            NavigationLink {
+                                VideoView(video: video)
+                            } label: {
+                                VideoCard(video: video)
+                            }
+                        }
+                    }
+                }
+                .frame(maxWidth: .infinity)
             }
-            ScrollView{
-                VideoCard(video: previewVideo)
-            }
-            .frame(maxWidth: .infinity)
+            .background(Color("AccentColor"))
+            .navigationBarHidden(true)
         }
-        .background(Color("AccentColor"))
     }
 }
 
